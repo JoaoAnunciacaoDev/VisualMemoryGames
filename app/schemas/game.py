55cb2d0806f9a3abc0a797abad8d45a23ce87a12
João Anunciação <js5283283@gmail.com>
2026-06-23 11:25:13 -0300

    Update validations game.py

--- app/schemas/game.py
+++ app/schemas/game.py
@@ -1,17 +1,14 @@
 import json
 from typing import Optional
-from pydantic import BaseModel, ConfigDict, Field, field_validator
+from pydantic import BaseModel, ConfigDict, Field, field_validator, HttpUrl
 from datetime import datetime
 from app.enums.game_status import GameStatus
-
-
-CURRENT_YEAR = datetime.now().year
 
 
 class GameBase(BaseModel):
     external_id: int
     title: str
-    cover_url: Optional[str] = None
+    cover_url: Optional[HttpUrl] = None
     release_year: int | None
     platforms: list[str]
     genres: list[str]
@@ -25,6 +22,7 @@ class GameResponse(GameBase):
     id: str
     
     model_config = ConfigDict(from_attributes=True)
+    
     @field_validator('platforms', 'genres', mode='before')
     @classmethod
     def parse_json_list(cls, v):
@@ -36,8 +34,17 @@ class GameResponse(GameBase):
 class UserGameBase(BaseModel):
     rating: Optional[int] = Field(default=None, ge=0, le=10)
     status: GameStatus = GameStatus.WANT_TO_PLAY
-    played_year: Optional[int] = Field(default=None, ge=1970, le=CURRENT_YEAR)
+    played_year: Optional[int] = Field(default=None, ge=1970)
     notes: Optional[str] = None
+    
+    @field_validator('played_year')
+    @classmethod
+    def validate_played_year(cls, value):
+        if value is not None:
+            current_year = datetime.now().year
+            if value > current_year:
+                raise ValueError(f"O ano jogado não pode ser maior que o ano atual ({current_year})")
+        return value
 
 
 class UserGameCreate(UserGameBase):
@@ -47,8 +54,17 @@ class UserGameCreate(UserGameBase):
 class UserGameUpdate(BaseModel):
     rating: Optional[int] = Field(default=None, ge=0, le=10)
     status: Optional[GameStatus] = None
-    played_year: Optional[int] = Field(default=None, ge=1970, le=CURRENT_YEAR)
+    played_year: Optional[int] = Field(default=None, ge=1970)
     notes: Optional[str] = None
+
+    @field_validator('played_year')
+    @classmethod
+    def validate_played_year(cls, value):
+        if value is not None:
+            current_year = datetime.now().year
+            if value > current_year:
+                raise ValueError(f"O ano jogado não pode ser maior que o ano atual ({current_year})")
+        return value
 
 
 class UserGameResponse(UserGameBase):
@@ -65,11 +81,11 @@ class LibraryGameResponse(BaseModel):
     game_id: str
     external_id: int
     title: str
-    cover_url: Optional[str] = None
+    cover_url: Optional[HttpUrl] = None
     release_year: Optional[int] = None
     rating: Optional[int] = None
-    status: str
-    played_year: Optional[int] = Field(default=None, ge=1970, le=CURRENT_YEAR)
+    status: GameStatus
+    played_year: Optional[int] = None
     notes: Optional[str] = None
 
     model_config = ConfigDict(from_attributes=True)
